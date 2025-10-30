3dac0de9a380b121f3e912444bd1726d50c2a3eb
Chaz <Chaz151515torres@gmail.com>
2025-10-30 13:03:40 -0500

    Updating db with user and data

--- model/base_db.sql
+++ model/base_db.sql
@@ -1,131 +1,79 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Oct 03, 2025 at 11:20 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+DROP DATABASE IF EXISTS `base_db`;
+CREATE DATABASE `base_db`;
+USE `base_db`;
 
-
--- Create the tech_support database
-DROP DATABASE IF EXISTS base_db;
-CREATE DATABASE base_db;
-USE base_db;
-
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `base_db`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `courses`
---
+-- Create table structure for "courses"
 
 CREATE TABLE `courses` (
-  `course_ID` int(10) UNSIGNED NOT NULL,
-  `course_professor` varchar(50) NOT NULL,
+  `course_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `course_prof` varchar(50) NOT NULL,
   `course_name` varchar(100) NOT NULL,
-  `course_number` varchar(10) NOT NULL,
-  `course_updated` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `course_no` varchar(10) NOT NULL,
+  `last_update` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`course_id`)
+);
 
---
--- Dumping data for table `courses`
---
+-- Insert generic data into "courses"
 
-INSERT INTO `courses` (`course_ID`, `course_professor`, `course_name`, `course_number`, `course_updated`) VALUES
-  (1,'Rose Wilson','Principles of Financial Accounting I','ACC2001','2025-10-03 21:18:02'),
-  (2,'Mary Simpson','Principles of Financial Accounting I','ACC2001','2025-10-03 21:18:02'),
-  (3,'Mary Simpson','Principles of Financial Accounting II','ACC2011','2025-10-03 21:18:02'),
-  (4,'Ken Smith','Microcomputer Applications','CGS1570','2025-10-03 21:18:02'),
-  (5,'Wayne Paulson','Microcomputer Applications','CGS1570','2025-10-03 21:18:02'),
-  (6,'Ted Eve','Introduction to Programming Logic','COP1000','2025-10-03 21:18:02'),
-  (7,'Cate Jackson','Introduction to Java Programming','COP2250','2025-10-03 21:18:02'),
-  (8,'Wayne Paulson','Data Structure (SQL)','COP2700','2025-10-03 21:18:02'),
-  (9,'David Lawson','Network Fundamentals','CTS1650','2025-10-03 21:18:02'),
-  (10,'Rose Ryan','Web Design I','DIG2100','2025-10-03 21:18:02'),
-  (11,'Ben Conner','Principles of Economics, Macro','ECO2013','2025-10-03 21:18:02'),
-  (12,'Mat Musk','Principles of Economics, Macro','ECO2013','2025-10-03 21:18:02'),
-  (13,'Mat Musk','Principles of Economics, Micro','ECO2023','2025-10-03 21:18:02'),
-  (14,'Cathy Patterson','Financial Management','FIN3400','2025-10-03 21:18:02'),
-  (15,'Ted Eve','SecDevOps','CIS4433','2025-10-03 21:18:02'),
-  (16,'Wayne Paulson','Agile Project Management','ISM4318','2025-10-03 21:18:02'),
-  (17,'Rose Ryan','Emerging Technologies','ISM4302','2025-10-03 21:18:02');
+INSERT INTO `courses` (`course_id`, `course_prof`, `course_name`, `course_no`, `last_update`) VALUES
+(1, 'Rose Wilson', 'Principles of Financial Accounting I', 'ACC2001', '2025-10-24 04:42:12'),
+(2, 'Mary Simpson', 'Principles of Financial Accounting I', 'ACC2001', '2025-10-24 04:42:12'),
+(3, 'Mary Simpson', 'Principles of Financial Accounting II', 'ACC2011', '2025-10-24 04:42:12'),
+(4, 'Ken Smith', 'Microcomputer Applications', 'CGS1570', '2025-10-24 04:42:12'),
+(5, 'Wayne Paulson', 'Microcomputer Applications', 'CGS1570', '2025-10-24 04:42:12'),
+(6, 'Ted Eve', 'Introduction to Programming Logic', 'COP1000', '2025-10-24 04:42:12'),
+(7, 'Cate Jackson', 'Introduction to Java Programming', 'COP2250', '2025-10-24 04:42:12'),
+(8, 'Wayne Paulson', 'Data Structure (SQL)', 'COP2700', '2025-10-24 04:42:12'),
+(9, 'David Lawson', 'Network Fundamentals', 'CTS1650', '2025-10-24 04:42:12'),
+(10, 'Rose Ryan', 'Web Design I', 'DIG2100', '2025-10-24 04:42:12'),
+(11, 'Ben Cole', 'Principles of Economics, Macro', 'ECO2013', '2025-10-24 04:42:12'),
+(12, 'Mat Musk', 'Principles of Economics, Macro', 'ECO2013', '2025-10-24 04:42:12'),
+(13, 'Mat Musk', 'Principles of Economics, Micro', 'ECO2023', '2025-10-24 04:42:12'),
+(14, 'Cathy Patterson', 'Financial Management', 'FIN3400', '2025-10-24 04:42:12'),
+(15, 'Ted Eve', 'SecDevOps', 'CIS4433', '2025-10-24 04:42:12'),
+(16, 'Wayne Paulson', 'Agile Project Management', 'ISM4318', '2025-10-24 04:42:12'),
+(17, 'Rose Ryan', 'Emerging Technologies', 'ISM4302', '2025-10-24 04:42:12');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `students`
---
+-- Create table structure for "students"
 
 CREATE TABLE `students` (
-  `student_ID` int(10) UNSIGNED NOT NULL,
-  `student_fname` varchar(50) NOT NULL,
-  `student_lname` varchar(50) NOT NULL,
-  `student_email` varchar(120) NOT NULL,
-  `student_major` varchar(100) NOT NULL,
-  `student_updated` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `stu_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `f_name` varchar(50) NOT NULL,
+  `l_name` varchar(50) NOT NULL,
+  `stu_email` varchar(120) NOT NULL UNIQUE,
+  `major` varchar(100) NOT NULL,
+  `last_update` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`stu_id`)
+);
 
---
--- Dumping data for table `students`
---
+-- Insert generic data into "students"
 
-INSERT INTO `students` (`student_ID`, `student_fname`, `student_lname`, `student_email`, `student_major`, `student_updated`) VALUES
-(1, 'Peter', 'Cullen', 'pcullen@my.gulfcoast.edu', '', '2025-10-03 21:18:02'),
-(2, 'Tom', 'Kenny', 'tkenny2@my.gulfcoast.edu', '', '2025-10-03 21:18:02'),
-(3, 'Frank', 'Welker', 'fwelker@my.gulfcoast.edu', '', '2025-10-03 21:18:02'),
-(4, 'Steve', 'Blum', 'sblum@my.gulfcoast.edu', '', '2025-10-03 21:18:02'),
-(5, 'Tabitha', 'St. Germain', 'tsgermain@my.gulfcoast.edu', '', '2025-10-03 21:18:02');
+INSERT INTO `students` (`stu_id`, `f_name`, `l_name`, `stu_email`, `major`, `last_update`) VALUES
+(1, 'Spike', 'Witwicky', 'switwicky@my.gulfcoast.edu', '', '2025-10-24 04:42:12'),
+(2, 'Johnathan', 'Smith', 'jsmith8@my.gulfcoast.edu', '', '2025-10-24 04:42:12'),
+(3, 'Kevin', 'Flynn', 'kflynn2@my.gulfcoast.edu', '', '2025-10-24 04:42:12'),
+(4, 'Claire', 'Redfield', 'credfield@my.gulfcoast.edu', '', '2025-10-24 04:42:12'),
+(5, 'David', 'Lightman', 'dlightman@my.gulfcoast.edu', '', '2025-10-24 04:42:12'),
+(6, 'Ellen', 'Ripley', 'eripley@my.gulfcoast.edu', '', '2025-10-24 04:42:12'),
+(7, 'Luz', 'Noceda', 'lnoceda@my.gulfcoast.edu', '', '2025-10-24 04:42:12'),
+(8, 'Barney', 'Calhoun', 'bcalhoun@my.gulfcoast.edu', '', '2025-10-24 04:42:12'),
+(9, 'Misa', 'Hayase', 'mhayase@my.gulfcoast.edu', '', '2025-10-24 04:42:12'),
+(10, 'Cliff', 'Steele', 'csteele2@my.gulfcoast.edu', '', '2025-10-24 04:42:12');
 
---
--- Indexes for dumped tables
---
+-- Create table structure for "logs"
 
---
--- Indexes for table `courses`
---
-ALTER TABLE `courses`
-  ADD PRIMARY KEY (`course_ID`),
-  ADD UNIQUE KEY `course_number` (`course_number`);
+CREATE TABLE `logs` (
+  `log_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `sign_in` datetime NOT NULL,
+  `sign_out` datetime NOT NULL,
+  `stu_id` int(10) UNSIGNED NOT NULL,
+  `course_id` int(10) UNSIGNED NOT NULL,
+  `log_updated` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`log_id`),
+  FOREIGN KEY (`stu_id`) REFERENCES students(`stu_id`),
+  FOREIGN KEY (`course_id`) REFERENCES courses(`course_id`)
+);
 
---
--- Indexes for table `students`
---
-ALTER TABLE `students`
-  ADD PRIMARY KEY (`student_ID`),
-  ADD UNIQUE KEY `student_email` (`student_email`);
+-- Create "ts_user"
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `courses`
---
-ALTER TABLE `courses`
-  MODIFY `course_ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `students`
---
-ALTER TABLE `students`
-  MODIFY `student_ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+GRANT SELECT, INSERT, UPDATE, DELETE ON * TO log_user@localhost IDENTIFIED BY 'pa55word';
