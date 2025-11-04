a8852e8c5aa7d22258d45978bef9d70807f4495c
jaykingjr <jking3@my.gulfcoast.edu>
2025-11-03 22:49:49 -0600

    bug fix

--- login/index.php
+++ login/index.php
@@ -27,7 +27,7 @@
 		onkeyup="validateUsername(this);"
 		required>
 		<span>@my.gulfcoast.edu</span>
-		<span id="usernameError" class="errorMsg"></span>
+		<br><span id="usernameError" class="errorMsg"></span>
 	</div>
 		</fieldset>
 	<br>
