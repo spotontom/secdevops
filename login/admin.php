3ca4948c29cf411072eb7cdac409bff4cee9604c
cjb1115 <cjb1115@my.gulfcoast.edu>
2025-11-06 12:56:17 -0600

    removed admin.php from login folder

--- login/admin.php
+++
@@ -1,6 +0,0 @@
-<?php include 'views/head.php'; ?>
-<main>
-    <h2>Sorry, the admin page is currently under construction.</h2>
-    <p>We'll finish it as quickly as we can. Thanks!</p>
-</main>
-<?php include 'views/footer.php'; ?>
