63b504aae213e9512b2a63ae049aa38a5c03e81b
cjb1115 <cjb1115@my.gulfcoast.edu>
2025-11-06 12:37:13 -0600

    created admin folder and created index.php.

---
+++ Admin/index.php
@@ -0,0 +1,7 @@
+<?php include 'views/head.php'; ?>
+<main>
+    <h2>Sorry, the admin page is currently under construction.</h2>
+    <p>We'll finish it as quickly as we can. Thanks!</p>
+    <p>TODO: Display first 10 lines of logs here</p>
+</main>
+<?php include 'views/footer.php'; ?>
