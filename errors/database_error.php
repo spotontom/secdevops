d0e115d69b444ac3641ed44f49defa2afffb3089
Chaz <Chaz151515torres@gmail.com>
2025-11-13 08:06:30 -0600

    error message update

--- errors/database_error.php
+++ errors/database_error.php
@@ -2,6 +2,6 @@
 <main>
     <h1>Database Error</h1>
     <p>There was an error connecting to the database.</p>
-    <p>Error message: <?php echo $error_message; ?></p>
+    <p>Please try refreshing the page. If error persist please contact us. (850)-999-9999</p>
 </main>
 <?php include '../view/footer.php'; ?>
