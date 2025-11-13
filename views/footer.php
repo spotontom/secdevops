5bb1d5573f0e550c4d0e7435f4c1f35137c342e4
Chaz <Chaz151515torres@gmail.com>
2025-11-13 08:16:32 -0600

    Footer link to admin folder

--- views/footer.php
+++ views/footer.php
@@ -1,5 +1,6 @@
 <footer>
 <h6>
-&#169; <abbr title="Gulf Coast State College">GCSC</abbr> - cis4433 - SecDevOps <?php echo date("Y"); ?>
+<a href="../Admin">&#169; </a>
+ <abbr title="Gulf Coast State College">GCSC</abbr> - cis4433 - SecDevOps <?php echo date("Y"); ?>
 </h6>
 </footer>
