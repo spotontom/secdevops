975b493335fce68e75cbab1b84dc83b193032a95
jaykingjr <jking3@my.gulfcoast.edu>
2025-11-20 17:07:49 -0600

    Update login.php
    Registered students do not need to reenter their Name. This is doen at the register.php.

--- login/login.php
+++ login/login.php
@@ -6,9 +6,10 @@ session_start();
 	Class:		cop4433
 	Project:	ACE Tutoring Lab
 	Author:		Jay King
-	Updated:	10-15-2025
+	Created:	10-15-2025
 	Filename:	login.php
--->
+	Updated:	11-20-2025 by Jay King
+	-->
 <html lang="en">
 <?php include '../views/head.php';?>
 <?php include '../model/database.php';?>
@@ -16,8 +17,8 @@ session_start();
 <body>
 <?php include '../views/header.php';?>
 <main>
-<form id="loginForm" autocomplete="on" method="post" action="proceed.php">
-	<h2>Welcome</h2>
+<h2>Welcome</h2>
+<form id="loginForm"autocomplete="on"method="post"action="proceed.php">
 	<fieldset>
 		<label title="Display only">Email:</label>
 			<input type="email"
@@ -26,22 +27,22 @@ session_start();
 			size="25"
 			value="<?php echo isset($_SESSION['emailInput']) ? $_SESSION['emailInput'] : ''; ?>"
 			readonly>
-		<br>
-		<br>
-		<label title="First name is a required entry">First name:</label>
-			<input type="text" size="25" onkeyup="validateFirstname(this)"
-			title="First name is a required entry"
-			placeholder = "John-example" id="firstNameInput" name="firstNameInput" required>
-		<p><span id="firstNameError" class="error-msg"></span></p>
-		<label title="Last name is a required entry">Last name:</label>
-			<input type="text" size="25" onkeyup="validateLastname(this)"
-			title="Last name is a required entry"
-			placeholder = "Smith-example" id="lastNameInput" name="lastNameInput" required>
-		<p><span id="lastNameError" class="error-msg"></span><p>
+		<br><br>
+		<label>First name:</label>
+			<input type="text"size="25"
+			value="<?php echo isset($_SESSION['student_fname']) ? $_SESSION['student_fname'] : ''; ?>"
+			readonly>
+		<br><br>
+		<label>Last name:</label>
+			<input type="text"size="25"
+			value="<?php echo isset($_SESSION['student_lname']) ? $_SESSION['student_lname'] : ''; ?>"
+			readonly>
+		<br><br>
 		<label title="Requires a selection">Course:</label>
 		<?php
-		$courses = get_courses();?>
-		<select id="selectCourseInput" name="selectCourseInput" required>
+			$courses = get_courses();
+		?>
+		<select id="selectCourseInput"name="selectCourseInput"required>
 			<option value="">Select course with professor</option>
 			<?php foreach ($courses as $course): ?>
 				<option value="<?php echo htmlspecialchars($course['course_ID']); ?>">
@@ -52,60 +53,11 @@ session_start();
 					?>
 				</option>
 			<?php endforeach; ?>
-		</select>
+ 		</select>
 	</fieldset>
 	<br>
-	<button type="submit" class ="btn1">Confirm</button>
+	<button type="submit"class ="btn1">Confirm</button>
 </form>
 </main>
 <?php include '../views/footer.php';?>
-<script>
-"use strict";
-const charList = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-// Get the input element and the display element
-const firstElement = document.getElementById('firstNameInput');
-const lastElement = document.getElementById('lastNameInput');
-const firstNameErrorDisplay  = document.getElementById("firstNameError");
-const lastNameErrorDisplay  = document.getElementById("lastNameError");
-// Add an 'input' event listener
-firstElement.addEventListener('input', function(e) {
-    const cursorPosition = this.selectionStart;
-    // Get the character that was just added/is at the position before the caret
-    // We get the character right before the current cursor position
-    const lastChar = this.value[cursorPosition - 1];
-	let errorDisplay = "";
-	if (lastChar ==" ") {
-	    this.value = this.value.trim();
-		errorDisplay = 'No spaces allowed';
-    // Check if the last typed character and remove it if not allowed
-    } else if (charList.indexOf(lastChar) < 0) {
-        // Update the input value to exclude the not allowed character
-        this.value = this.value.slice(0, cursorPosition - 1) + this.value.slice(cursorPosition);
-        // Reposition the cursor correctly after removal
-        this.selectionStart = this.selectionEnd = cursorPosition - 1;
-		errorDisplay = 'Removed '+lastChar+'. It is not allowed.';
-    }
-	firstNameErrorDisplay.textContent=errorDisplay; // display message
-});
-// Add an 'input' event listener
-lastElement.addEventListener('input', function(e) {
-    const cursorPosition = this.selectionStart;
-    // Get the character that was just added/is at the position before the caret
-    // We get the character right before the current cursor position
-    const lastChar = this.value[cursorPosition - 1];
-	let errorDisplay = "";
-	if (lastChar ==" ") {
-	    this.value = this.value.trim();
-		errorDisplay = 'No spaces allowed';
-    // Check if the last typed character and remove it if not allowed
-    } else if (charList.indexOf(lastChar) < 0) {
-        // Update the input value to exclude the not allowed character
-        this.value = this.value.slice(0, cursorPosition - 1) + this.value.slice(cursorPosition);
-        // Reposition the cursor correctly after removal
-        this.selectionStart = this.selectionEnd = cursorPosition - 1;
-		errorDisplay = 'Removed '+lastChar+'. It is not allowed.';
-    }
-	lastNameErrorDisplay.textContent=errorDisplay; // display message
-});
-</script>
 </html>
