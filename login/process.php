55f3b36377c5a50aab8976453c92252fcf7c1076
jaykingjr <jking3@my.gulfcoast.edu>
2025-11-21 18:50:55 -0600

    Update process.php

--- login/process.php
+++ login/process.php
@@ -1,5 +1,9 @@
 <?php
 session_start();
+if ($_SESSION['statusFlag'] == 1 ) {
+	$_SESSION['statusFlag'] = 2;
+}
+$_SESSION['student_ID'] = 0;
 $_SESSION['student_fname'] = "none";
 $_SESSION['student_lname'] = "none";
 $studentID = 0; // if no student is found with that email
@@ -14,6 +18,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Check if a record was found and return the student details
     if ($studentData) {
         $studentID = (int) $studentData['student_ID'];
+		$_SESSION['student_ID'] = $studentID;		
 		$_SESSION['student_fname'] = $studentData['student_fname'];
 		$_SESSION['student_lname'] = $studentData['student_lname'];
     }
@@ -22,25 +27,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 			$existLogRecord = get_log_by_student($studentID);
 			if ($existLogRecord) {
 				// incomplete record found, update it with signout datetime
-// temporary debuging, to be removed
-die ('<div style="color:red;
-font-size: 4.0vw;
-font-weight: 600;
-background-color: #eeeeee;
-padding: 2rem;
-margin: 2rem auto 0 auto;
-border: 0.1rem solid red;
-width: 80%;">'
-. 'Found incomplete log record, to signout (update record) studentID '
-. $studentID
-. ' name '
-. $_SESSION['student_fname']
-. ' '
-. $_SESSION['student_lname']
-. ' '
-. $emailInput
-. '</div>');	
-				// example is studentID 30 smccoy2
+				// example is studentID 30 Sheila McCoy smccoy2
 				$sql = "UPDATE log SET log_signout = NOW() WHERE log_ID = ?";
 				$updateStmt = $db->prepare($sql);
 				$updateStmt->execute([$existLogRecord['log_ID']]);
@@ -66,7 +53,15 @@ width: 80%;">'
 			. $e->getMessage() . '</div>');
 		}
     } else {
-        // No student found with that email
+		
+		// To register new student for tutoring
+		if ($_SESSION['statusFlag'] == 2 ) {
+			$_SESSION['statusFlag'] = 0;
+			// To register new student for tutoring
+			header('Location: register.php');
+			exit;
+		}
+		// No student found with that email
 		$_SESSION['statusFlag'] = 1;
 		header('Location: index.php');
 		exit;
