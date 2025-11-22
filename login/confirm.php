1058370460b7ec1c7015b6d4a12bef65fecc86bc
jaykingjr <jking3@my.gulfcoast.edu>
2025-11-21 19:45:53 -0600

    Update confirm.php

--- login/confirm.php
+++ login/confirm.php
@@ -1,12 +1,27 @@
 <?php
 session_start();
+$emailInput = "";
+$studentFname = "";
+$studentLname = "";
+if (isset($_SESSION['emailInput'])) {
+	$emailInput = $_SESSION['emailInput'];
+}
+if (isset($_SESSION['studentFname'])) {
+	$studentFname = $_SESSION['studentFname'];
+}
+if (isset($_SESSION['$studentLname'])) {
+	$$studentLname = $_SESSION['$studentLname'];
+}
+$_SESSION = array();	// Unset all session variables
+session_destroy();		// Destroy the session	
 ?>
 <!DOCTYPE html>
 <!--
 	Class:		cop4433
 	Project:	ACE Tutoring Lab
 	Author:		Jay King
-	Updated:	10-15-2025
+	Created:	10-15-2025
+	Updated:	11-20-2025
 	Filename:	confirm.php
 -->
 <html lang="en">
@@ -17,8 +32,8 @@ session_start();
 <main>
 <h2>Confirmed</h2>
 <p>
-You are signed in with email:
-<?php echo isset($_SESSION['emailInput']) ? $_SESSION['emailInput'] : ''; ?>
+<?php echo $studentFname." ".$studentLname." ".$emailInput; ?>
+You are signed in.
 </p>
 </main>
 <?php include '../views/footer.php';?>
