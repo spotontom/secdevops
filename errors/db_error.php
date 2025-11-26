e513c7d649e5d1d497342582acb20bf97d2c773e
jaykingjr <jking3@my.gulfcoast.edu>
2025-11-26 11:12:40 -0600

    document

--- errors/db_error.php
+++ errors/db_error.php
@@ -1,4 +1,14 @@
 <?php
+/*
+	Class:		cop4433
+	Project:	ACE Tutoring Lab
+	Author:		Jay King
+	Created:	11-20-2025
+	Filename:	db_error.php
+	
+	Temporary file, to be used to display database errors during debugging.
+	Will be replaced by error.php or database_error.php with its logging and error display
+*/
 function db_error($e,$message) {
 	die('<div style="color:red;
 	font-size: 4.0vw;
