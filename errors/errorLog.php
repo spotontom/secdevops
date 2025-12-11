9abda9d33de087dd49c2b381c21119775f508b69
jaykingjr <jking3@my.gulfcoast.edu>
2025-12-11 02:14:22 -0600

    error log bug fox

--- errors/errorLog.php
+++ errors/errorLog.php
@@ -2,8 +2,9 @@
 /*
 	Class:		cop4433
 	Project:	ACE Tutoring Lab
-	Author:		Jay King
-	Created:	11-20-2025
+	Author:		Chaz
+	Created:	11-19-2025
+	Updated:	12-07-2025 Jay King
 	Filename:	errorLog.php
 */
 if (session_status() === PHP_SESSION_NONE) {
@@ -20,7 +21,29 @@ function errorLog($errorMsg, $e=null) {
 		error_log($e->getTraceAsString().PHP_EOL,3, "../errors/error.log");
 	}
 	$_SESSION['errorLog'] = $errorMsg;
-	header("Location: ../errors/error.php");
-	exit();
+	// problems with header allready called
+	// header('Location: ../errors/error.php');
+	// exit();
+	
+die ('<div style="color:black;
+	font-size: 4.0vw;
+	font-weight: 600;
+    text-align: center;
+	border-radius: 1vw;
+    border: 0.4vw solid red;
+    box-shadow: 0 0.5vw 1.1vw rgba(0, 0, 0, 0.15);
+    background-color: #f3f3f3;
+    width: 70vw;
+    padding: 0.5vw 0.5vw 2vw 0.5vw;
+    margin:5vh auto 10vh auto;">'
+. '<h2>ERROR in '
+. $_SESSION['errorLog']
+. '</h2><br>'
+. 'There was an error connecting to the database.'
+. '<br><br>'
+. 'Please refresh and try again.'
+. '<br><br>'
+. 'The error.log has captured this error.'
+. '</div>');	
 }
 ?>
